5c42ea9fbd1b33466dd2d4b033965c1c13d96772
Abdul Ahad Khan <ahadaak231@gmail.com>
2023-10-20 03:22:24 +0400

    removed favicon

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <h1>Testing continuous Deploy</h1>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="app">
+    Hello from AWS
+  </div>
 </template>
 
 <style scoped>
